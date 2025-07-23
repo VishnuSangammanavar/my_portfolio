1fb58ff155391c7bc878647fd0ca010e16cc3a15
VishnuSangammanavar <vishnusangammanavar25@gmail.com>
2025-07-23 20:25:41 +0530

    resume link update

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -12,7 +12,7 @@ const Hero = () => {
                             <h1>Hi, I'm <br />Vishnu Sangammanavar</h1>
                             <span>Eat -&gt; Sleep -&gt; Code -&gt; Repeat</span>
                             <p>Hello! I am 21 Current student looking to join the workforce to gain real-world experience. I completed my BCA at KLE Society's College of BCA RLSI, Belagavi with aggregate CGPA of 7.9. Ability to complete tasks on time in both individual and team settings. Dedicated worker with excellent computer skills and time management. Very much eager to learn new things.</p>
-                            <a href="https://drive.google.com/file/d/1SKkoP6PKECDXhfIF3K9aoyFhNoPHkXkF/view?usp=sharing" className="btn btn-outline-danger hire">Get my resume</a>
+                            <a href="https://drive.google.com/file/d/1-o8Hf3dwfaB2-rwUhQIvtY1EIfLozqOG/view?usp=drive_link" className="btn btn-outline-danger hire">Get my resume</a>
                         </div>
                         <div className="col-lg-4 col-md-6 image">
                             <img src="./img/myimg.jpg" alt="My image" />
